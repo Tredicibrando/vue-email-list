02f998c7aaf35769dfdbf14455c639cbfaf342d6
Tredicibrando <nicolosigiov@gmail.com>
2022-03-04 16:34:30 +0100

    finito

--- js/app.js
+++ js/app.js
@@ -13,27 +13,18 @@ const app = new Vue({
             for (let index = 0; index < 10; index++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
-                   
+            
                    this.email = res.data.response;
                    console.log(this.email);
                    this.emails.push(this.email);
                    
                 })
-                console.log(this.emails);
-                // this.emails.push(this.userMail);
-                // console.log(this.emails)
-                // const element = this.emails;
-                // this.emails.push(this.email)
-                // console.log(this.emails);
             };
         }
     },
 
-    // created() {
-    //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    //     .then((res) => {
-    //         this.email = res.data.response
-    //         console.log(this.email)
-    //     })
-    // },
+    created(){
+        this.fillEmails();
+    }
+
 });
