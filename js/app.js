dd1e90a3668b276b92bb2076d01d28238de79074
Tredicibrando <nicolosigiov@gmail.com>
2022-03-04 16:10:00 +0100

    per_alessio

--- js/app.js
+++ js/app.js
@@ -3,9 +3,37 @@ console.log('ciao');
 const app = new Vue({
     el: '#app',
     data: {
-        
-    },
-    methods: {
+        email: null,
+        emails : [],
 
     },
+    methods: {
+        
+        fillEmails: function(){
+            for (let index = 0; index < 10; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                   
+                   this.email = res.data.response;
+                   console.log(this.email);
+                   this.emails.push(this.email);
+                   
+                })
+                console.log(this.emails);
+                // this.emails.push(this.userMail);
+                // console.log(this.emails)
+                // const element = this.emails;
+                // this.emails.push(this.email)
+                // console.log(this.emails);
+            };
+        }
+    },
+
+    // created() {
+    //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    //     .then((res) => {
+    //         this.email = res.data.response
+    //         console.log(this.email)
+    //     })
+    // },
 });
